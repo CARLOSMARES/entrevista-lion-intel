b29da5c1f63d087b73087e204d10f67a0ef2e32b
CARLOSMARES <46010587+CARLOSMARES@users.noreply.github.com>
2024-05-18 13:31:45 -0600

    Se agregan parametros a la tabla de User

--- server/src/api/entities/User.ts
+++ server/src/api/entities/User.ts
@@ -2,12 +2,23 @@ import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class User {
+
   @PrimaryGeneratedColumn()
   id!: number;
 
   @Column()
-  name!: string;
+  firstName!: string;
+
+  @Column()
+  lastName!: string;
 
   @Column()
   email!: string;
+
+  @Column()
+  password!: string;
+
+  @Column()
+  rol!: string;
+
 }
